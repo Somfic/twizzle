76a244f834ccf498dfa8677c1646cb058bd8b329
Kaan <kaanmol@outlook.com>
2022-03-21 03:29:22 +0100

    Added commands to import

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,13 @@
 import Leave from "./leave";
 import Ping from "./ping";
 import Search from "./search";
-import YouTube from "./youtube";
 import Command from "./command";
 import Skip from "./skip";
-import Join from "./join";
-import Spotify from "./spotify";
+import Playlist from "./playlist";
+import Track from "./track";
+import Clear from "./clear";
+import Shuffle from "./shuffle";
+import Play from "./play";
+import Pause from "./pause";
 
-export { Leave, Ping, Search, YouTube, Skip, Command, Join, Spotify };
+export { Leave, Ping, Search, Skip, Command, Playlist, Track, Clear, Shuffle, Play, Pause };
