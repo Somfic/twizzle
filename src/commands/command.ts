993b844f73915118e570956a8cea0558d3656b04
Kaan <kaanmol@outlook.com>
2022-03-21 02:17:53 +0100

    Added interaction transformer

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -9,6 +9,17 @@ export default class Command {
         this.command = command;
     }
 
+    protected transformInteraction(interaction: CommandInteraction) {
+        const guild = interaction?.client?.guilds?.cache.get(interaction?.guildId ?? "0")
+        const member = guild?.members?.cache.get(interaction?.member?.user?.id ?? "0");
+        const channel = member?.voice?.channel;
+
+        return {
+            guild, member, channel
+        };
+        
+    }
+
     public async handler(interaction: CommandInteraction): Promise<void> {}
 
     public getCommandJSON(): RESTPostAPIApplicationCommandsJSONBody {
